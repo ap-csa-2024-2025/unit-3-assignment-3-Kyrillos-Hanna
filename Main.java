f9649d642793fb0fdbb35e2879d9b0e53d9df65f
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-11-28 22:42:32 -0800

    finished unit 3 assignment 3

--- Main.java
+++ Main.java
@@ -4,13 +4,54 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // TODO: Problem 1
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter 2 integers");
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+    double ratio = (double) num1 / num2;
 
+    if (num2 == 0) {
+      System.out.println("Cannot divide by 0");
+    } else {
+      if (ratio > 1 && ratio <= 8) {
+        System.out.println("Ratio OK");
+      }
+    }
 
-    // TODO: Problem 2
+    System.out.println("Enter 2 integers");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
 
+    if (b == 0) {
+      System.out.println("Cannot divide by 0");
+    } else {
+      if (a % b == 0) {
+        System.out.println("Is a factor");
+      }
+    }
 
-    // TODO: Problem 3
+    System.out.println("Enter an integer in the fifties");
+    int c = sc.nextInt();
+    if (c < 50 || c >= 60) {
+      System.out.println("Number not in the fifties");
+      c = 55;
+    }
+    System.out.println("Number is " + c);
+
+    System.out.println("Enter 2 integers");
+    int x = sc.nextInt();
+    int y = sc.nextInt();
+
+   if (y <= 9 || (x > 2 && x * y > 10)) {
+    System.out.println("pass");
+   }
+
+   Rectangle r1 = new Rectangle(2, 6);
+   Rectangle r2 = new Rectangle(1, 4);
+   System.out.println(rectEqual(r1, r2));
+   r2.setLength(2);
+   r2.setWidth(6);
+   System.out.println(rectEqual(r1, r2));
   }
 
   // TODO: In-class assignment
@@ -20,7 +61,10 @@ public class Main
   // and if their lengths are equal.
   public static boolean rectEqual(Rectangle r1, Rectangle r2)
   {
-    // implement solution here
-    return false;
+    double length1 = r1.getLength();
+    double width1 = r1.getWidth();
+    double length2 = r2.getLength();
+    double width2 = r2.getWidth();
+    return (length1 == length2 && width1 == width2);
   }
 }
